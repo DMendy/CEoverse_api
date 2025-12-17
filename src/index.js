5dc75b46d30b4d0e1026765892f91dcb6e88e23a
Doryan MENDY <dmendy@maisonsdumonde.com>
2025-12-17 03:50:40 +0100

    ajout de la route de mailing

--- src/index.js
+++ src/index.js
@@ -2,29 +2,35 @@ import { app } from './app.js';
 import { env } from './config/env.js';
 import { logger } from './utils/logger.js';
 
-const server = app.listen(env.port, () => {
-  logger.info('CEOverse API démarrée', {
-    port: env.port,
-    env: env.nodeEnv,
+let server = null;
+
+if (!process.env.VERCEL) {
+  server = app.listen(env.port, () => {
+    logger.info('CEOverse API démarrée', {
+      port: env.port,
+      env: env.nodeEnv,
+    });
   });
-});
 
-const gracefulShutdown = (signal) => {
-  logger.info(`Signal ${signal} reçu, arrêt du serveur...`);
-  server.close(() => {
-    process.exit(0);
+  const gracefulShutdown = (signal) => {
+    logger.info(`Signal ${signal} reçu, arrêt du serveur...`);
+    server.close(() => {
+      process.exit(0);
+    });
+  };
+
+  ['SIGINT', 'SIGTERM'].forEach((signal) => {
+    process.on(signal, () => gracefulShutdown(signal));
   });
-};
 
-['SIGINT', 'SIGTERM'].forEach((signal) => {
-  process.on(signal, () => gracefulShutdown(signal));
-});
+  process.on('unhandledRejection', (error) => {
+    logger.error('Rejection non gérée', { error: error?.message });
+  });
 
-process.on('unhandledRejection', (error) => {
-  logger.error('Rejection non gérée', { error: error?.message });
-});
+  process.on('uncaughtException', (error) => {
+    logger.error('Exception non gérée', { error: error?.message });
+    process.exit(1);
+  });
+}
 
-process.on('uncaughtException', (error) => {
-  logger.error('Exception non gérée', { error: error?.message });
-  process.exit(1);
-});
+export default app;
